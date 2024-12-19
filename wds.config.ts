9b30e62898895e81d6d2cf3d262398e3f719bda9
vanyauhalin <vanyauhalin@gmail.com>
2024-12-19 21:13:12 +0400

    move the wds config to the const

--- wds.config.ts
+++ wds.config.ts
@@ -1,8 +1,10 @@
 import {type DevServerConfig} from "@web/dev-server"
 import {esbuildPlugin} from "@web/dev-server-esbuild"
 
-export default {
+const config: DevServerConfig = {
   nodeResolve: true,
   plugins: [esbuildPlugin({ts: true})],
   watch: true,
-} satisfies DevServerConfig
+}
+
+export default config
